ab0d3beda5d03fbfcdac4cb61c7b43f36be27729
a23 <takahiroatsumi0517@gmail.com>
2023-08-09 02:45:59 +0900

    Add pagination

--- frontend/src/views/blog/Home.js
+++ frontend/src/views/blog/Home.js
@@ -9,7 +9,9 @@ class HomeComponent extends Component {
     return (
         <div className="container mx-auto px-10 mb-8">
           <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-            <PostList/>
+            <div className="lg:col-span-8 col-span-1">
+              <PostList/>
+            </div>
             <div className="lg:col-span-4 col-span-1">
               <div className="lg:sticky relative top-8">
                 <Categories/>

30a1d60895c257594bff0e021f90d5924edd62ae
a23 <takahiroatsumi0517@gmail.com>
2023-08-09 02:44:59 +0900

    Remove redux

--- frontend/src/redux/Reducers.js
+++ frontend/src/redux/Reducers.js
@@ -1,13 +1,6 @@
 import { combineReducers } from "redux";
-import settings from "./settings/Reducer";
-import routesReducer from './routes/Reducer';
-import times from './times/Reducer';
-
 
 const Reducers = combineReducers({
-  settings,
-  routesReducer,
-  times,
 });
 
 export default Reducers;

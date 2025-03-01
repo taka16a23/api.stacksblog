ce2a4bfc5d30d0821f1535d15f343790025d289d
a23 <takahiroatsumi0517@gmail.com>
2025-03-01 14:58:44 +0900

    Refactoring

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -4,6 +4,7 @@ import ReactPaginate from 'react-paginate';
 import { withTranslation } from 'react-i18next';
 
 import PostCard from 'components/PostCard';
+import PostNotFound from 'components/PostNotFound';
 import { ServiceFactory } from 'services';
 
 
@@ -64,19 +65,7 @@ class PostList extends Component {
   render() {
     if(this.state.isLoaded === true && this.list_post_ids.length <= 0) {
       return (
-        <div>
-          <div className="text-center text-white">
-            <h3 className="font-bold">
-              {this.props.t("POST NOT FOUND !")}
-            </h3>
-            <p className="mt-4 mb-4 font-bold">
-              {this.props.t("YOU SEEM TO BE TRYING TO FIND HIS WAY HOME")}
-            </p>
-            <a href="/" className="bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:ring-pink-300 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-pink-600 dark:hover:bg-pink-700 focus:outline-none dark:focus:ring-pink-800">
-              <span className="font-bold">{this.props.t("Back to home")}</span>
-            </a>
-          </div>
-        </div>
+        <PostNotFound/>
       )
     }
 
